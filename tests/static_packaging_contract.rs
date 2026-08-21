d9c0ae9aca87638173a2287129ffbabadf56a75a
LIghtJUNction <lightjunction.me@gmail.com>
2026-08-21 10:31:19 +0800

    fix: anchor package test helper discovery

--- tests/static_packaging_contract.rs
+++ tests/static_packaging_contract.rs
@@ -6,3 +6,8 @@ fn vcs_pkgver_keeps_the_semantic_prefix_without_a_tag() {
     assert!(PKGBUILD.contains("printf '0.1.0.r%s.%s'"));
     assert!(!PKGBUILD.contains("printf 'r%s.%s'"));
 }
+
+#[test]
+fn package_checks_anchor_project_helper_discovery_to_the_checkout() {
+    assert!(PKGBUILD.contains("export VWA_PROJECT_ROOT=\"$srcdir/$_pkgsrc\""));
+}
